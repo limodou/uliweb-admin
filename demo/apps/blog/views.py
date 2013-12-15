f3ca94e4a92150ff9b9be6948ecc52aed90b2cd1
limodou <limodou@gmail.com>
2013-12-15 20:47:24 +0800

    Improve blog demo

--- demo/apps/blog/views.py
+++ demo/apps/blog/views.py
@@ -17,3 +17,19 @@ class BlogView(object):
         categories = self.C.all()
         
         return {'blogs':blogs, 'categories':categories}
+    
+    @expose('<int:id>')
+    def view(self, id):
+        obj = self.M.get(int(id))
+        return {'blog':obj}
+    
+    def category(self, id):
+        page = int(request.GET.get('page', 1) or 1)
+        size = 10
+        offset = (page - 1)*size
+        
+        blogs = self.M.filter(self.M.c.category==int(id)).order_by(self.M.c.created_time.desc()).offset(offset).limit(size)
+        category = self.C.get(int(id))
+        
+        return {'blogs':blogs, 'category':category}
+        
